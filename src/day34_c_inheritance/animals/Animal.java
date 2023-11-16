122d2b5088dcd90c629d0d2a4c3ae6e192055c3c
AslanAslanli <143632955+AslanAslanli@users.noreply.github.com>
2023-11-16 07:04:53 +0400

    nested loop intro

--- src/day34_c_inheritance/animals/Animal.java
+++ src/day34_c_inheritance/animals/Animal.java
@@ -1,7 +1,7 @@
 package day34_c_inheritance.animals;
 
 public class Animal {
-    private String species;
+    String species;
     int numOfLegs;
     public void walk(){
         System.out.println(species + " is walking");
